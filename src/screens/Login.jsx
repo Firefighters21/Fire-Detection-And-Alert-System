1267d8ab3a064a9278bda566d4b1eaf389cd9748
Emmanuel Tweneboah <emmanueltweneboah71@gmail.com>
2021-03-29 15:27:05 +0000

    sign in page

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/core';
-import { View, Text, StyleSheet, Button,TextInput,input } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput, } from 'react-native';
 
 const LoginPage = () => {
 	const navigation = useNavigation();
@@ -8,11 +8,24 @@ const LoginPage = () => {
 	return (
 		<View style={styles.root}>
 			<Text style={styles.text}>SIGN IN TO YOUR ACCOUNT</Text>
-			
+			<TextInput style={styles.input}
+			placeholder = 'Username'
+			/> 
+
+			 <TextInput style={styles.input}
+			placeholder = 'password'
+			/>
+			<Text style={styles.text} >Already An Existing User?</Text>
+
+			<Button title="click here to sign up" />
+	
 			<Button title="Go to home page" onPress={() => navigation.navigate('auth')} />
 		</View>
+
+		
 	);
 };
+
 
 const styles = StyleSheet.create({
 	root: {
@@ -27,6 +40,14 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold',
 		color: '#000',
 	},
+
+	input : {
+		borderWidth :1, 
+		width : 300,
+		textAlign : 'center',
+		fontSize: 20,
+		backgroundColor: '#ffffff'
+	}	
 
 });
 
