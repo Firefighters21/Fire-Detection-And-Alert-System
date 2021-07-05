f5052e8d7f6c9bd5c1129aa8989aacf4ea335543
Francis Gyimah <fgyimah883@gmail.com>
2021-07-05 15:25:37 +0000

    sd

---
+++ src/styles/Bg.jsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Bg = styled(View)`
+	height: 100%;
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+`;
+
+export default Bg;
