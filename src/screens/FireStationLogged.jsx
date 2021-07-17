6ad21f756d9ff2142d27d3aea1caf8bcb6350e6f
Emmanuel Tweneboah <emmanueltweneboah71@gmail.com>
2021-07-17 14:25:27 +0000

    Map configurations

--- src/screens/FireStationLogged.jsx
+++ src/screens/FireStationLogged.jsx
@@ -1,42 +1,102 @@
-import React from 'react';
+import * as React from "react"
 import { useNavigation } from '@react-navigation/core';
-import { View , StyleSheet} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { Dimensions, StyleSheet, Text, View } from "react-native"
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
+import MapView, { Callout, Circle, Marker, PROVIDER_GOOGLE } from "react-native-maps"
 
 
 
 
-
-const FireStationLogged = () => {
-	const navigation = useNavigation();
-
-     
+const FireStationLogged = () =>{
+  const navigation = useNavigation();
+	const [ pin, setPin ] = React.useState({
+		latitude: 6.700071,
+		longitude: -1.630783
+	})
+	const [ region, setRegion ] = React.useState({
+		latitude: 6.700071,
+		longitude: -1.630783,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421
+	})
 
 	return (
-        <View style={styles.container} >
-        
-
-            <StatusBar style="fade" />
-            
-        </View>
-		
-		
-	);
-};
+		<View style={{ marginTop: 50, flex: 1 }}>
+			<GooglePlacesAutocomplete
+				placeholder="Search"
+				fetchDetails={true}
+				GooglePlacesSearchQuery={{
+					rankby: "distance"
+				}}
+				onPress={(data, details = null) => {
+					// 'details' is provided when fetchDetails = true
+					console.log(data, details)
+					setRegion({
+						latitude: details.geometry.location.lat,
+						longitude: details.geometry.location.lng,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421
+					})
+				}}
+				query={{
+					key: "KEY",
+					language: "en",
+					components: "country:us",
+					types: "establishment",
+					radius: 30000,
+					location: `${region.latitude}, ${region.longitude}`
+				}}
+				styles={{
+					container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+					listView: { backgroundColor: "white" }
+				}}
+			/>
+			<MapView
+				style={styles.map}
+				showsUserLocation={true}
+				initialRegion={{
+					latitude: 6.700071,
+					longitude: -1.630783,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421
+				}}
+				provider={PROVIDER_GOOGLE}
+			>
+				<Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+				<Marker
+					coordinate={pin}
+					draggable={true}
+					onDragStart={(e) => {
+						console.log("Drag start", e.nativeEvent.coordinates)
+					}}
+					onDragEnd={(e) => {
+						setPin({
+							latitude: e.nativeEvent.coordinate.latitude,
+							longitude: e.nativeEvent.coordinate.longitude
+						})
+					}}
+				>
+					<Callout>
+						<Text>I'm here</Text>
+					</Callout>
+				</Marker>
+				<Circle center={pin} radius={1000} />
+			</MapView>
+		</View>
+	)
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-    },
-    image: {
-      flex: 1,
-      resizeMode: 'cover',
-      justifyContent: 'center',
-    }
-    
-  });
-
-
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height
+	}
+})
 
 export default FireStationLogged;
